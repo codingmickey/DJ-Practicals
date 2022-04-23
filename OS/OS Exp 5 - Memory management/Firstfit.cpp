90ec8edcf166f8cf9d42593316a54dff4fbccbde
Kartik <kjmickey003@gmail.com>
2022-04-24 00:48:35 +0530

    Complete First fit

---
+++ OS/OS Exp 5 - Memory management/Firstfit.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int noOfPartitions;
+    // cout << "Enter the number of patitions: ";
+    cin >> noOfPartitions;
+    int partitionMemory[noOfPartitions], tempPMemory[noOfPartitions];
+    for (int i = 0; i < noOfPartitions; i++)
+    {
+        // cout << "Partition " << i + 1 << ": ";
+        cin >> partitionMemory[i];
+        tempPMemory[i] = partitionMemory[i];
+    }
+    int noOfProcesses;
+    // cout << "Enter the number of processes: ";
+    cin >> noOfProcesses;
+    int processesMemory[noOfProcesses];
+    for (int i = 0; i < noOfProcesses; i++)
+    {
+        // cout << "Partition " << i + 1 << ": ";
+        cin >> processesMemory[i];
+    }
+    string firstFit[noOfPartitions], notAllocatedProcesses;
+    for (int i = 0; i < noOfPartitions; i++)
+    {
+        firstFit[i] = "X";
+    }
+    for (int i = 0; i < noOfProcesses; i++)
+    {
+        bool isAlloc = false;
+        for (int j = 0; j < noOfPartitions; j++)
+        {
+            if (processesMemory[i] <= partitionMemory[j])
+            {
+                partitionMemory[j] -= processesMemory[i];
+                if (firstFit[j] == "X")
+                {
+
+                    firstFit[j] = "P" + to_string(i + 1);
+                }
+                else
+                {
+                    firstFit[j] += ", P" + to_string(i + 1);
+                }
+                isAlloc = true;
+                break;
+            }
+        }
+        if (!isAlloc)
+        {
+            notAllocatedProcesses += "P" + to_string(i + 1);
+        }
+    }
+    cout << "Partitions\t\tFirst Fit\n";
+    for (int i = 0; i < noOfPartitions; i++)
+    {
+        cout << tempPMemory[i] << "\t\t\t" << firstFit[i] << "\n";
+    }
+    if (notAllocatedProcesses.empty())
+    {
+        cout << "There are no unallocated processes!\n";
+    }
+    else
+    {
+        cout << "The unallocated processes are: " << notAllocatedProcesses << "\n";
+    }
+
+    return 0;
+}
